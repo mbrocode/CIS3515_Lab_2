8e54645993b0a95395b26581a1142c31b2b85b35
mbrocode <48998172+mbrocode@users.noreply.github.com>
2019-09-18 12:08:34 -0400

    Initial commit

---
+++ app/src/main/java/edu/temple/formactivity/MainActivity.java
@@ -0,0 +1,51 @@
+package edu.temple.formactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textInputName;
+    TextView textInputEmail;
+    TextView textInputPassword;
+    TextView textInputPwCnfrm;
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textInputName = findViewById(R.id.inputName);
+        textInputEmail = findViewById(R.id.inputEmail);
+        textInputPassword = findViewById(R.id.inputPassword);
+        textInputPwCnfrm = findViewById(R.id.inputPwCnfrm);
+
+        findViewById(R.id.registerButton).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(textInputName.getEditableText().toString().isEmpty() ||
+                        textInputEmail.getEditableText().toString().isEmpty() ||
+                        textInputPassword.getEditableText().toString().isEmpty() ||
+                        textInputPwCnfrm.getEditableText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter all information", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if((textInputPassword.getEditableText().toString().compareTo(textInputPwCnfrm.getEditableText().toString())) != 0){
+                    Toast.makeText(MainActivity.this, "Password confirmation failed", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Toast.makeText(MainActivity.this, "You have successfully registered!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
+
